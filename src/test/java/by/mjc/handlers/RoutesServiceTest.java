adce91653ce0d3353702abd436b771c3ed6bf7cd
Andrey Radzkov <andrey.radzkov@gmail.com>
2020-12-04 11:02:49 +0300

    Jupiter upgrade

--- src/test/java/by/mjc/handlers/RoutesServiceTest.java
+++ src/test/java/by/mjc/handlers/RoutesServiceTest.java
@@ -8,30 +8,28 @@ import com.amazonaws.services.dynamodbv2.local.embedded.DynamoDBEmbedded;
 import com.amazonaws.services.dynamodbv2.local.shared.access.AmazonDynamoDBLocal;
 import com.amazonaws.services.dynamodbv2.model.*;
 import init.AwsDynamoDbLocalTestUtils;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
-
 public class RoutesServiceTest {
     private static AmazonDynamoDBLocal amazonDynamoDBLocal;
 
-    @Before
-    public void init() {
+    @BeforeAll
+    public static void init() {
         AwsDynamoDbLocalTestUtils.initSqLite();
         amazonDynamoDBLocal = DynamoDBEmbedded.create();
         AmazonDynamoDB amazonDynamoDB = amazonDynamoDBLocal.amazonDynamoDB();
         createTable(amazonDynamoDB, "Routes", "id");
     }
 
-    @AfterClass
+    @AfterAll
     public static void tearDownClass() {
         amazonDynamoDBLocal.shutdown();
     }
@@ -49,10 +47,10 @@ public class RoutesServiceTest {
         SearchRoutesRequest request = new SearchRoutesRequest();
         request.setTags(List.of("tag1"));
         List<Route> routes = routesService.getRoutesByTags(request);
-        assertThat(routes.size(), is(1));
+        Assertions.assertEquals(routes.size(), 1);
     }
 
-    private static CreateTableResult createTable(AmazonDynamoDB ddb, String tableName, String hashKeyName) {
+    private static void createTable(AmazonDynamoDB ddb, String tableName, String hashKeyName) {
         List<AttributeDefinition> attributeDefinitions = new ArrayList<>();
         attributeDefinitions.add(new AttributeDefinition(hashKeyName, ScalarAttributeType.S));
 
@@ -68,6 +66,6 @@ public class RoutesServiceTest {
                         .withKeySchema(ks)
                         .withProvisionedThroughput(provisionedthroughput);
 
-        return ddb.createTable(request);
+        ddb.createTable(request);
     }
 }
