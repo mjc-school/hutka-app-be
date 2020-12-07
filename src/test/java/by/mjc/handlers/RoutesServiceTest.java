77b44c1414a32e07a372dbfc365a33224117d154
Andrey Radzkov <andrey.radzkov@gmail.com>
2020-12-07 15:26:39 +0300

    Added test scenarios

--- src/test/java/by/mjc/handlers/RoutesServiceTest.java
+++ src/test/java/by/mjc/handlers/RoutesServiceTest.java
@@ -9,24 +9,30 @@ import com.amazonaws.services.dynamodbv2.local.shared.access.AmazonDynamoDBLocal
 import com.amazonaws.services.dynamodbv2.model.*;
 import init.AwsDynamoDbLocalTestUtils;
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RoutesServiceTest {
     private static AmazonDynamoDBLocal amazonDynamoDBLocal;
 
     @BeforeAll
-    public static void init() {
+    public static void init() throws FileNotFoundException, URISyntaxException {
         AwsDynamoDbLocalTestUtils.initSqLite();
         amazonDynamoDBLocal = DynamoDBEmbedded.create();
         AmazonDynamoDB amazonDynamoDB = amazonDynamoDBLocal.amazonDynamoDB();
         createTable(amazonDynamoDB, "Routes", "id");
+        List<List<String>> records = loadData("./data.csv");
+        saveData(records);
     }
 
     @AfterAll
@@ -34,20 +40,17 @@ public class RoutesServiceTest {
         amazonDynamoDBLocal.shutdown();
     }
 
-    @Test
-    public void test() {
-        RoutesService routesService = new RoutesService(amazonDynamoDBLocal.amazonDynamoDB());
-        Route route = new Route();
-        route.setKml("kml");
-        Set<String> tags = new HashSet<>();
-        tags.add("tag1");
-        route.setTags(tags);
-        routesService.saveRoute(route);
+    @ParameterizedTest(name = "#{index} - match search?")
+    @MethodSource("loadScenarios")
+    public void test(List<String> scenario) {
 
+        RoutesService routesService = new RoutesService(amazonDynamoDBLocal.amazonDynamoDB());
         SearchRoutesRequest request = new SearchRoutesRequest();
-        request.setTags(List.of("tag1"));
+        request.setTags(scenario.subList(0, scenario.size() - 1));
         List<Route> routes = routesService.getRoutesByTags(request);
-        Assertions.assertEquals(routes.size(), 1);
+        List<String> idsToExpect = Arrays.stream(scenario.get(scenario.size() - 1).split(" "))
+                .filter(id -> !id.equals("null")).collect(Collectors.toList());
+        assertEquals(idsToExpect, routes.stream().map(Route::getId).collect(Collectors.toList()));
     }
 
     private static void createTable(AmazonDynamoDB ddb, String tableName, String hashKeyName) {
@@ -68,4 +71,43 @@ public class RoutesServiceTest {
 
         ddb.createTable(request);
     }
+
+    private static List<List<String>> loadScenarios() throws FileNotFoundException, URISyntaxException {
+        return loadData("./scenarios.csv");
+    }
+
+    private static List<List<String>> loadData(String path) throws FileNotFoundException, URISyntaxException {
+        List<List<String>> rows = new ArrayList<>();
+        URL resource = RoutesServiceTest.class.getClassLoader().getResource(path);
+        try (Scanner scanner = new Scanner(new File(resource.toURI()))) {
+            while (scanner.hasNextLine()) {
+                rows.add(getRecordFromLine(scanner.nextLine()));
+            }
+        }
+        return rows;
+    }
+
+    private static List<String> getRecordFromLine(String line) {
+        List<String> values = new ArrayList<>();
+        try (Scanner rowScanner = new Scanner(line)) {
+            rowScanner.useDelimiter(",");
+            while (rowScanner.hasNext()) {
+                values.add(rowScanner.next());
+            }
+        }
+        return values;
+    }
+
+
+    private static void saveData(List<List<String>> records) {
+        RoutesService routesService = new RoutesService(amazonDynamoDBLocal.amazonDynamoDB());
+        records.forEach(record -> {
+            Route route = new Route();
+            route.setId(record.get(0));
+            route.setKml("kml");
+            route.setTags(new HashSet<>(record.subList(1, record.size())));
+            routesService.saveRoute(route);
+        });
+    }
+
 }
